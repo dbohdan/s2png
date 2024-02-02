022fe243b2d8ccdce5794c4b44279344c962fb43
D. Bohdan <dbohdan@dbohdan.com>
2024-02-02 12:21:06 +0000

    feat(corrupt.sh): improve CLI
    Ignore a ShellCheck error.
    
    [skip ci]

--- scripts/corrupt.sh
+++ scripts/corrupt.sh
@@ -3,13 +3,34 @@
 # This requires GraphicsMagick or ImageMagick.
 # The original file is not affected. Try it out on text.
 
-set -e
+set -eu
+
+usage() {
+    echo "usage: $0 input format [quality] [s2png-arg ...]"
+    echo 'The format can be, e.g., "gif" or "jpeg".'
+}
+
+if [ "$#" -lt 2 ]; then
+    usage
+    exit 2
+fi
+
+for arg in "$@"; do
+    if [ "$arg" = -h ] || [ "$arg" = --help ]; then
+        usage
+        exit 0
+    fi
+done
 
 input_file="$1"
 format="$2"
-quality="$3"
-[ "$quality" != "" ] && shift 3 && s2png_args="$*"
-
+quality="${3:-100}"
+if [ "$#" -ge 3 ]; then
+    shift 3
+else
+    shift 2
+fi
+s2png_args="$*"
 
 if command -v gm > /dev/null; then
     convert="gm convert"
@@ -21,25 +42,20 @@ else
 fi
 
 if [ ! -e "$input_file" ]; then
-    if [ "$input_file" != "" ]; then
-         echo "Not found: \"$1\""
-    fi
-    echo "usage: $0 input format [quality] [s2png-args...]"
-    echo 'The format can be, e.g., "gif" or "jpeg".'
+    echo "Not found: \"$1\""
     exit 1
 fi
-
-if [ "$quality" = "" ]; then
-    quality=100
-fi
-
 
 temp_png=/tmp/corruptsh-temp.png
 temp_lossy="/tmp/corruptsh-temp.$format"
 
+clean_up() {
+    rm "$temp_png" "$temp_lossy"
+}
+trap clean_up EXIT HUP INT QUIT TERM
+
+# shellcheck disable=SC2086
 s2png "$input_file" $s2png_args -e -o "$temp_png"
 $convert "$temp_png" -quality "$quality" "$temp_lossy"
 $convert "$temp_lossy" "$temp_png"
 s2png "$temp_png" -d -o /dev/stdout
-
-rm "$temp_png" "$temp_lossy"
